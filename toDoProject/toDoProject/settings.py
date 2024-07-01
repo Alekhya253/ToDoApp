c0e0c4af43dc257f9f4c282aa07590aebb224db8
Alekhya253 <20wh1a05b6@bvrithyderabad.edu.in>
2024-07-01 19:19:57 +0530

    vercel-changes

--- toDoProject/toDoProject/settings.py
+++ toDoProject/toDoProject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh"]
+ALLOWED_HOSTS = ["127.0.0.1", "https://to-do-app-iota-ten.vercel.app/", ".now.sh"]
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
